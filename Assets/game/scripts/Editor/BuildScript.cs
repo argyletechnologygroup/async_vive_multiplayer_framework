0cde9477ba4baa4a44a1940237905412cb197a02
Ian T. Small <ian.small@thekeepstudios.com>
2016-10-01 22:27:28 -0400

    Added separate build items for each executable type

--- Assets/game/scripts/Editor/BuildScript.cs
+++ Assets/game/scripts/Editor/BuildScript.cs
@@ -5,21 +5,77 @@ using System.Collections;
 
 public class BuildScript
 {
-    [MenuItem("MyTools/Make Separate Builds")]
-    public static void BuildGame ()
+    internal class BuildPaths
+    {
+        readonly string executableName;
+        readonly string[] scenes;
+
+        public BuildPaths(string executableName, string[] scenes)
+        {
+            this.executableName = executableName;
+            this.scenes = scenes;
+        }
+
+        public string Executable
+        {
+            get { return executableName; }
+        }
+
+        public string[] Scenes
+        {
+            get { return scenes; }
+        }
+    }
+
+    private static BuildPaths vrClientPaths = new BuildPaths("Client_VR.exe", new string[] { "Assets/game/scenes/connect-menu_vr.unity" });
+    private static BuildPaths clientPaths = new BuildPaths("Client.exe", new string[] { "Assets/game/scenes/connect-menu_nonvr.unity" });
+    private static BuildPaths serverPaths = new BuildPaths("Server.exe", new string[] { "Assets/game/scenes/connect-menu_server.unity" });
+
+    [MenuItem("Bulid/Build All")]
+    public static void BuildGame()
     {
         string path = EditorUtility.SaveFolderPanel("Choose Location of Built Games", "", "");
-        string[] levelsVR = new string[] {"Assets/game/scenes/connect-menu_vr.unity"};
-        string[] levelsNonVR = new string[] { "Assets/game/scenes/connect-menu_nonvr.unity" };
-        string[] levelsServer = new string[] { "Assets/game/scenes/connect-menu_server.unity" };
+        BuildServer(path);
+        BuildVRClient(path);
+        BuildClient(path);
+    }
 
+    [MenuItem("Bulid/Build Server")]
+    public static void BuildServer()
+    {
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Server", "", "");
+        BuildServer(path);
+    }
+
+    [MenuItem("Bulid/Build Client")]
+    public static void BuildClient()
+    {
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Client", "", "");
+        BuildClient(path);
+    }
+
+    [MenuItem("Bulid/Build VR Client")]
+    public static void BuildVRClient()
+    {
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built VR Client", "", "");
+        BuildVRClient(path);
+    }
+
+    private static void BuildServer(string path)
+    {
         //Build VR
-        BuildPipeline.BuildPlayer(levelsVR, path + "/Game_VR.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+        BuildPipeline.BuildPlayer( serverPaths.Scenes, path + "/" + serverPaths.Executable, BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
 
-        //Build non VR
-        BuildPipeline.BuildPlayer(levelsNonVR, path + "/Game_NonVR.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
-        
-        //Build Server
-        BuildPipeline.BuildPlayer(levelsServer, path + "/Game_Server.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    private static void BuildVRClient(string path)
+    {
+        //Build VR
+        BuildPipeline.BuildPlayer( vrClientPaths.Scenes, path + "/" + vrClientPaths.Executable, BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+
+    private static void BuildClient(string path)
+    {
+        //Build VR
+        BuildPipeline.BuildPlayer( clientPaths.Scenes, path + "/" + clientPaths.Executable, BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 }
